4547dbb18ce3c0f7cf8243982e907679c43de8a5
Marie <marieflechon@gmail.com>
2024-04-23 21:45:46 +0200

    add files -model

---
+++ Models/Model.cs
@@ -0,0 +1,84 @@
+﻿using RemoteInterface.Observables;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace RemoteInterface.Models
+{ 
+
+    public class Model : ObservableObject
+    {
+        // --- Attributes ---
+        // Prepare options to indent JSON Files
+        private JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+
+        public Settings settings { get; set; }
+        private string settingsFilePath { get; set; }
+        private ObservableCollection<Work> Works { get; set;}
+        public ObservableCollection<Work> works {
+            get {
+                return Works; 
+            }
+            set
+            {
+                if(Works != value)
+                {
+                    Works = value;
+                    OnPropertyChanged("works");
+                }
+            }
+        }
+
+
+        // --- Constructor ---
+        public Model()
+        { 
+            // Initialize Work List
+            works = new ObservableCollection<Work>();
+
+            // Initialize Settings
+            settingsFilePath = "./Settings.json";
+            this.settings = Settings.GetInstance();
+            LoadSettings();
+        }
+
+
+        // Load Settings (at the beginning of the program)
+        public void LoadSettings()
+        {
+            // Check if backupWorkSave.json File exists
+            if (File.Exists(settingsFilePath))
+            {
+                try
+                {
+                    // Read Works from JSON File (from ./BackupWorkSave.json) (use Work() constructor)
+                    this.settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(this.settingsFilePath));
+                }
+                catch
+                {
+                    // Return Error Code
+                    MessageBox.Show(Languages.Language.loadSettingsError);
+                }
+            }
+            else
+            {
+                // Create Settings File
+                SaveSettings();
+            }
+        }
+
+        // Save Settings
+        public void SaveSettings()
+        {
+            // Write Work list into JSON file (at ./BackupWorkSave.json)
+            File.WriteAllText(this.settingsFilePath, JsonSerializer.Serialize(this.settings, this.jsonOptions));
+        }
+    }
+}
---
+++ Models/Work.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using RemoteInterface.Models;
+using RemoteInterface.Observables;
+
+namespace RemoteInterface.Models
+{
+    public class Work : ObservableObject
+    {
+        // --- Attributes ---
+        public string name { get; set; }
+        private int Progress { get; set; }
+        public int progress {
+            get { return Progress; }
+            set {
+                if (Progress != value)
+                {
+                    Progress = value;
+                    OnPropertyChanged("progress");
+                }
+            }
+        }
+        public string ColorProgressBar { get; set; }
+        public string colorProgressBar
+        {
+            get { return ColorProgressBar; }
+            set
+            {
+                if (ColorProgressBar != value)
+                {
+                    ColorProgressBar = value;
+                    OnPropertyChanged("colorProgressBar");
+                }
+            }
+        }
+    }
+}
